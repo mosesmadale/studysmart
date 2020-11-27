9388a1b2151a98e4567dc4f46b8ae529ee81aac4
mosesmadale <66674160+mosesmadale@users.noreply.github.com>
2020-11-27 22:33:42 +0300

    Add files via upload

--- ins.js
+++ ins.js
@@ -1,89 +1,60 @@
-let slides = [];
- let num = 1;
+//start of javascript
 
- let fetchedNumber = JSON.parse(localStorage.getItem('slideNumber'));
+//selection of all elements
+let shop = document.querySelectorAll('.button-mini-max');
 
+//selection of shop
+let filteredShop = document.querySelector('.button-list');
 
-while(fetchedNumber>=num) {
-    slides.push('Slide'+num+'.JPG');
-    num++;
-} 
-
-function addSlideName(){
-    let array = localStorage.getItem('directory').split('/');
-    let base = array[0];
-    let tail = array[1];
-
-
-    document.querySelector('.name').innerHTML = `${base} - ${tail}`;
+document.querySelector('#input').oninput = () => {
+    filter(document.querySelector('#input').value);
 }
 
-addSlideName();
+function filter(criteria) {
+    filteredShop.innerHTML = '';
+    shop.forEach(e => {
+
+
+        console.log(e.lastElementChild);
+
+        let raw = [];
+        criteria.split('').forEach(e => {
+            raw.push(e.toLowerCase());
+        })
+
+
+        if (raw.length == 0) {
+            filteredShop.innerHTML = '';
+            shop.forEach(e => {
+                filteredShop.innerHTML += e.outerHTML;
+            })
+
+        }
+        let i = 0;
+        let verify = 0;
+
+
+        let comp = [];
+        e.firstElementChild.firstElementChild.innerHTML.split('').forEach(e => {
+            comp.push(e.toLowerCase());
+        })
+        let comp2 = [];
+        e.firstElementChild.lastElementChild.innerHTML.split('').forEach(e => {
+            comp2.push(e.toLowerCase());
+        })
 
 
 
+        while (i < raw.length) {
+            //code
+            if (raw[i] == comp[i] || raw[i] == comp2[i]) {
+                verify++;
+            }
+            if (verify == raw.length) {
+                filteredShop.innerHTML += e.outerHTML;
+            }
+            i++;
+        }
 
-let currentSlide = 0;
-
-let canvas = document.getElementById('canvas');
-
-console.log(localStorage.getItem('directory'))
-canvas.setAttribute('src', `../library/${localStorage.getItem('directory')}/${slides[currentSlide]}`);
-document.querySelector('.slide-info').innerHTML = `Slide: ${currentSlide+1} / ${slides.length}`;
-
-function changeSlide(direction) {
-    if (direction == 'left' && currentSlide != 0) {
-        currentSlide--;
-        canvas.setAttribute('src', `../library/${localStorage.getItem('directory')}/${slides[currentSlide]}`);
-        document.querySelector('.slide-info').innerHTML = `Slide: ${currentSlide+1} / ${slides.length}`;
-    }
-    if (direction == 'right' && currentSlide != slides.length - 1) {
-        currentSlide++;
-        canvas.setAttribute('src', `../library/${localStorage.getItem('directory')}/${slides[currentSlide]}`);
-        document.querySelector('.slide-info').innerHTML = `Slide: ${currentSlide+1} / ${slides.length}`;
-    }
-}
-
-document.querySelector('.left').addEventListener('click', () => {
-    changeSlide('left')
-})
-document.querySelector('.right').addEventListener('click', () => {
-    changeSlide('right')
-})
-
-//listerner for fullscreen action
-
-function addFullScreen(){
-   /*$('.overlay').css('display','block');
-   $('.slide-pad').css('position','fixed')
-   $('.slide-pad').css('top','50%')
-   $('.slide-pad').css('left','50%')
-   $('.slide-pad').css('transform','translate(-50%, -50%)')
-   $('.slide-pad').css('z-index','2')*/
-
-   //$('.slide-controls').css('height','fit-content')
-   //$('#canvas').css('width','0%')
-
-   canvas.requestFullscreen()
-}
-
-function removeOverlay(){
-    $('.overlay').css('display','none');
-}
-
-document.querySelector('#btn-fullscreen').addEventListener('click',()=>{
-    addFullScreen();
-})
-
-$('.overlay').click(function(){
-    removeOverlay();
-})
-
-window.onkeydown = function(e){
-//if statement
-    if(e.key == 'ArrowLeft'){
-        changeSlide('left')
-    }else if(e.key == 'ArrowRight'){
-        changeSlide('right')
-    }
+    })
 }
